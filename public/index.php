b5d2c8210ed0edcbb1b8ba8cd5af181dba58d764
julius <j.benas91@gmail.com>
2019-10-22 21:39:34 +0300

    Removed error display

--- public/index.php
+++ public/index.php
@@ -5,6 +5,5 @@ use MailerTiny\Core\Application;
 require __DIR__.'/../vendor/autoload.php';
 $config = require __DIR__.'/../configs/database.php';
 
-ini_set('display_errors', 1);
 Application::start($config);
 
